8df8715c7a9a1acd2e712a1a214de93ee2cff92b
NVAN0 <ghostnv70@gmail.com>
2026-05-18 13:48:18 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+// Smooth Scroll
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
+    });
+  });
+});
+
+// Navbar Blur Effect
+window.addEventListener("scroll", () => {
+  const navbar = document.querySelector(".navbar");
+
+  if (window.scrollY > 50) {
+    navbar.style.background = "rgba(10,10,10,0.9)";
+  } else {
+    navbar.style.background = "rgba(18,18,18,0.7)";
+  }
+});
